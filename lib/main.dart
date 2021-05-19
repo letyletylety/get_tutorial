f5af59e4462a493b133b07ad1e2e3190d806bd04
Lety <taej0127@gmail.com>
2021-05-19 12:20:10 +0900

    init lesson2
    todo

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(GetMaterialApp(
-    initialRoute: '/home',
-    getPages: [
-      GetPage(name: '/home', page: () => HomeView(), binding: HomeBinding()),
-    ],
-  ));
-}
-
-class HomeBinding implements Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => HomeController());
-  }
-}
-
-class HomeController extends GetxController {
-  var count = 0.obs;
-  void increment() => count++;
-}
-
-class Home extends GetView<HomeController> {
-  @override
-  Widget build(context) => Scaffold(
-      appBar: AppBar(title: Text("counter")),
-      body: Center(
-        child: Obx(() => Text("${controller.counter}")),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: controller.increment,
-      ));
+  runApp(TodosApp());
 }
